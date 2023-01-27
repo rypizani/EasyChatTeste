2def7aee7402a62adaee56a5f5f0913644c3c6f4
rypizani <rypizani@gmail.com>
2023-01-27 12:00:24 -0300

    teste

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ io.on('connection', socket => {
      });
 });
 
-const PORT = 3333 || process.env.PORT;
+const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
